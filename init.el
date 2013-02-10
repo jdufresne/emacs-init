06da5685a951e9ecb7debd6757b7539ae1810033
Jon Dufresne <jon@jondufresne.org>
2013-02-09 17:25:45 -0800

    Moved all elisp to init.el

--- init.el
+++ init.el
@@ -1,6 +1,12 @@
-;;;; emacs init file
-;; Jon Dufresne <jon@jondufresne.org>
+;;; init.el --- Emacs initialization file
 
+;; Author: Jon Dufresne <jon@jondufresne.org>
+
+;;; Commentary:
+
+;; Initialize Emacs the way I like it.
+
+;;; Code:
 
 ;; Basic config
 (menu-bar-mode 0)
@@ -49,7 +55,6 @@
 (add-to-list 'same-window-regexps "\\*grep\\*<[[:digit:]]+>")
 (add-to-list 'same-window-buffer-names "*SQL*")
 
-
 ;; Style
 (setq-default tab-width 4)
 
@@ -77,42 +82,110 @@
 (add-hook 'sql-interactive-mode-hook (lambda () (linum-mode 0)))
 (add-hook 'shell-mode-hook (lambda () (linum-mode 0)))
 
-;; Load from external files
-(add-to-list 'load-path "~/.emacs.d/")
+;; Remove annoying keys
+(global-unset-key (kbd "<insert>"))
+(global-unset-key (kbd "C-z"))
+(global-unset-key (kbd "C-x C-z"))
 
-;; Handle in external files
-(require 'current-buffer)
-(require 'region-rigidly)
-(require 'smart-lines)
-(require 'keys)
-(require 'unfill)
-(require 'project)
+;; Always kill the current buffer without asking
+(global-set-key (kbd "C-x k") (lambda () (interactive) (kill-buffer)))
 
+(global-set-key (kbd "C-m") 'newline-and-indent)
+(global-set-key (kbd "C-c C-g") 'grep)
+(global-set-key (kbd "<f11>") 'shell)
+(global-set-key (kbd "<f12>") 'sql-mysql)
+
+(defun rename-current-buffer-file (new-file-name)
+  "Rename file backed by current buffer to NEW-FILE-NAME."
+  (interactive "FNew name: ")
+  (let ((name (buffer-name))
+        (file-name (buffer-file-name))
+        (new-name (file-name-nondirectory new-file-name)))
+    (if (not (and file-name (file-exists-p file-name)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (error "A buffer named '%s' already exists!" new-name)
+        (rename-file file-name new-file-name 1)
+        (rename-buffer new-name)
+        (set-visited-file-name new-file-name)
+        (set-buffer-modified-p nil)
+        (message "File '%s' successfully renamed to '%s'" name new-name)))))
+
+(defun delete-current-buffer-file ()
+  "Delete file backed by current buffer."
+  (interactive)
+  (let ((file-name (buffer-file-name)))
+    (if (not (and file-name (file-exists-p file-name)))
+        (kill-buffer)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (kill-buffer)
+        (delete-file file-name)
+        (message "File '%s' successfully removed" file-name)))))
+
+(global-set-key (kbd "C-x C-r") 'rename-current-buffer-file)
+(global-set-key (kbd "C-x C-k") 'delete-current-buffer-file)
+
+(defun smart-beginning-of-line ()
+  "Move point to first non-whitespace or the beginning of the line."
+  (interactive)
+  (let ((pos (point)))
+    (back-to-indentation)
+    (if (= pos (point))
+        (beginning-of-line))))
+
+(defun smart-end-of-line ()
+  "Move point to last non-whitespace or the end of the line."
+  (interactive)
+  (let ((pos (point)))
+    (end-of-line)
+    (re-search-backward "[^ \t]" (line-end-position 0) t)
+    (forward-char)
+    (if (= pos (point))
+        (end-of-line))))
+
+(global-set-key (kbd "<home>") 'smart-beginning-of-line)
+(global-set-key (kbd "C-a") 'smart-beginning-of-line)
+(global-set-key (kbd "<end>") 'smart-end-of-line)
+(global-set-key (kbd "C-e") 'smart-end-of-line)
+
+(defun unfill-paragraph ()
+  "Unfill paragraph at or after point."
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil)))
+
+(defun unfill-region ()
+  "Unfill each of the paragraphs in the region."
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-region (region-beginning) (region-end) nil)))
+
+(global-set-key (kbd "M-Q") 'unfill-paragraph)
+(global-set-key (kbd "C-M-Q") 'unfill-region)
 
 ;; libs
 (eval-and-compile
   (require 'package)
 
   (defun require-packages (&rest packages)
-	(mapc (lambda (package)
-			(when (not (package-installed-p package))
-			  (package-install package)))
-		  packages))
-
+    (dolist (package packages)
+      (when (not (package-installed-p package))
+        (package-install package))))
 
   (package-initialize)
   (add-to-list 'package-archives
-			   '("marmalade" . "http://marmalade-repo.org/packages/"))
+               '("marmalade" . "http://marmalade-repo.org/packages/"))
   (add-to-list 'package-archives
-			   '("melpa" . "http://melpa.milkbox.net/packages/"))
-  (package-refresh-contents)
+               '("melpa" . "http://melpa.milkbox.net/packages/"))
+  (when (not (file-exists-p package-user-dir))
+    (package-refresh-contents))
 
   (require-packages 'browse-kill-ring
-					'flycheck
-					'grep-a-lot
-					'php-mode
-					'smart-tabs-mode
-					'undo-tree))
+                    'flycheck
+                    'grep-a-lot
+                    'php-mode
+                    'smart-tabs-mode
+                    'undo-tree))
 
 (require 'browse-kill-ring)
 (browse-kill-ring-default-keybindings)

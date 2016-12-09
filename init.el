d97d6769046b36cb3150d15dbca72ae1f52e195c
Jon Dufresne <jon.dufresne@gmail.com>
2016-12-09 14:15:54 -0800

    remove customize

--- init.el
+++ init.el
@@ -291,19 +291,3 @@ Turn off LINUM-MODE, as the buffer can be extremely large."
 
 ;; Additional extensions.
 (require 'myproject)
-
-;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (yaml-mode web-beautify use-package undo-tree s projectile pony-mode pip-requirements php-mode mwim magit less-css-mode grep-a-lot flycheck flx-ido editorconfig diff-hl crontab-mode apache-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )

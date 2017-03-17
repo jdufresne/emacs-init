10cf2f8121886ce5bfb3353c71cd0468137078da
Jon Dufresne <jon.dufresne@gmail.com>
2017-03-17 09:38:38 -0700

    Replace obsolete projectile-global-mode with projectile-mode

--- init.el
+++ init.el
@@ -292,7 +292,7 @@ Turn off LINUM-MODE, as the buffer can be extremely large."
             (add-to-list 'projectile-globally-ignored-file-suffixes ".min.js")
             (add-to-list 'projectile-globally-ignored-files "ansible.log")
             (add-to-list 'projectile-globally-ignored-files "urlconf.php")
-            (projectile-global-mode 1)))
+            (projectile-mode 1)))
 
 (use-package s)
 

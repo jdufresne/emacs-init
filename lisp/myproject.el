5058b6a87078b01e923b76b95d2559c54ce12c61
Jon Dufresne <jon.dufresne@gmail.com>
2019-04-13 16:37:12 -0700

    Add support to run development servers inside Emacs

--- lisp/myproject.el
+++ lisp/myproject.el
@@ -22,9 +22,57 @@
   (dired (projectile-expand-root (concat
                                   "venv/lib/python" (python-version) "/site-packages/django"))))
 
+(defun run-development-server (buffer-name make-target)
+  "Run development server.
+
+Server is starting by running make target MAKE-TARGET in the
+project's root directory. The development server's output will
+appear in buffer BUFFER-NAME."
+  (let ((default-directory (projectile-project-root))
+        (compilation-read-command nil)
+        (compilation-buffer-name-function (lambda (name-of-mode) buffer-name)))
+    (compile (concat "make " make-target) t)))
+
+(defun run-django ()
+  "Run Django development server."
+  (interactive)
+  (run-development-server "*Django*" "run-django"))
+
+(defun run-php ()
+  "Run PHP development server."
+  (interactive)
+  (run-development-server "*PHP*" "run-php"))
+
+(defun run-webpack ()
+  "Run webpack development server."
+  (interactive)
+  (run-development-server "*webpack*" "run-webpack"))
+
+(defun run-development-servers ()
+  "Run all development servers."
+  (interactive)
+  (kill-development-servers)
+  (run-webpack)
+  (run-php)
+  (run-django))
+
+(defun kill-buffer-if-exists (buffer-name)
+  "Kill buffer named BUFFER-NAME if it exists."
+  (let ((buffer (get-buffer buffer-name)))
+    (when buffer
+      (kill-buffer buffer))))
+
+(defun kill-development-servers ()
+  "Kill all development server buffers."
+  (interactive)
+  (let ((kill-buffer-query-functions nil))
+    (dolist (buffer-name '("*webpack*" "*PHP*" "*Django*"))
+      (kill-buffer-if-exists buffer-name))))
+
 ;;; Key bindings:
 
 (global-set-key (kbd "<f10>") #'goto-django)
+(global-set-key (kbd "<f5>") #'run-development-servers)
 
 (provide 'myproject)
 ;;; myproject.el ends here

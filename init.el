34a985004aa5d0a3142756131a11fce9d2adb492
Jon Dufresne <jon.dufresne@gmail.com>
2020-10-26 08:03:13 -0700

    Disable emacs lock files

--- init.el
+++ init.el
@@ -27,6 +27,7 @@
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
+(setq create-lockfiles nil)
 (setq inhibit-splash-screen t)
 (setq initial-scratch-message nil)
 (setq ring-bell-function 'ignore)

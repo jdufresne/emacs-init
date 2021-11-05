734a9812b8617bf6cde2254bc9bdd97ca07d4f64
Jon Dufresne <jon.dufresne@gmail.com>
2021-11-04 20:29:24 -0700

    Replace MySQL mode with PostgreSQL

--- init.el
+++ init.el
@@ -123,20 +123,14 @@
     (sql-product-interactive product))
   (pop-to-buffer (sql-find-sqli-buffer product)))
 
-(setq sql-mysql-login-params '((database :default "literacyfootprints"))
-      sql-postgres-login-params ())
-
-(defun project-sql-mysql ()
-  "Run MySQL with default database for current project."
-  (interactive)
-  (project-sql 'mysql))
+(setq sql-postgres-login-params '((database :default "literacyfootprints")))
 
 (defun project-sql-postgres ()
   "Run PostgreSQL with default database for current project."
   (interactive)
   (project-sql 'postgres))
 
-(global-set-key (kbd "<f12>") #'project-sql-mysql)
+(global-set-key (kbd "<f12>") #'project-sql-postgres)
 
 (defun init-sql-mode ()
   "Initialize SQL-MODE."

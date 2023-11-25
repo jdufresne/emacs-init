07c4866be8dc8f42421e839206f0c3927b8dbffe
Jon Dufresne <jon.dufresne@gmail.com>
2023-11-25 08:16:52 -0800

    Add tree-sitter grammars

--- init.el
+++ init.el
@@ -211,6 +211,29 @@
 
 (load "~/.emacs.d/env" t)
 
+;; tree-sitter
+
+(setq treesit-language-source-alist
+      '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+        (css "https://github.com/tree-sitter/tree-sitter-css")
+        (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+        (html "https://github.com/tree-sitter/tree-sitter-html")
+        (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+        (json "https://github.com/tree-sitter/tree-sitter-json")
+        (make "https://github.com/alemuller/tree-sitter-make")
+        (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+        (python "https://github.com/tree-sitter/tree-sitter-python")
+        (ruby "https://github.com/tree-sitter/tree-sitter-ruby")
+        (toml "https://github.com/tree-sitter/tree-sitter-toml")
+        (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+        (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+        (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+
+(dolist (item treesit-language-source-alist)
+  (let ((lang (car item)))
+    (unless (treesit-language-available-p 'python)
+      (treesit-install-language-grammar lang))))
+
 ;; Third party libraries.
 
 (require 'package)
